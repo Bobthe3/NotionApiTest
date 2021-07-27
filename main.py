77d8c893b46901f97b5d2a02e4c1055d68f2d6ab
Devan velji <devanvelji@gmail.com>
2021-07-26 22:02:53 -0700

    set env variables

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
+import os
 
-url="https://api.notion.com/v1/pages"
-es=requests.post(url)
 
-print(es)
+#os.environ['NOTION_DATABASE_ID'] = 'ef8accf7a16c497a81afa2118df45b79'
+#os.environ['NOTION_KEY']= 'secret_TQ6WumAUtMsdw2iBZyKw7t8zp84a7jsE9LHxjX914r6'
+#os.environ['NOTION_PAGE_ID'] = '26fe2df1da8249b2b739012730cefc72'
+
+# url="https://api.notion.com/v1/pages"
+# es=requests.post(url, headers="Authorization: Bearer"+str())
+
+# print(es)
+print(os.environ['NOTION_KEY'])
